8007fd25c1b1f04a37e5b37375fcaa4d4723c671
Balan Vlad <cybyvlad@gmail.com>
2011-05-25 16:34:59 +0300

    changes

--- logParser.ps1
+++ logParser.ps1
@@ -4,4 +4,3 @@ $filePath = "c:\Users\bav\Documents\IISExpress\Logs\EVT2.log"
 .\LogParser.exe -i:W3C "SELECT TOP 25  uri-stem as Url, COUNT(*) As Hits  FROM $filePath GROUP BY uri-stem  ORDER By Hits DESC" -o:CSV
 .\LogParser.exe -i:W3C "SELECT TOP 25  uri-stem as URL, MAX(time-taken) As Max, MIN(time-taken) As Min, Avg(time-taken) As Average FROM $filePath GROUP BY URL ORDER By Average DESC" -o:CSV
 .\LogParser.exe -i:W3C "SELECT  User-Agent As UserAgent, COUNT(*) as Hits  FROM $filePath GROUP BY UserAgent ORDER BY Hits DESC" -o:CSV
-
